9af828dc4c4291a3a3d958910daf87768b53460d
tug69403 <>
2018-09-19 08:32:46 -0400

    Made edits to addPayment(not sure if that is allowed) and cancel to make it return the same coins that were inserted so far

--- src/paystation/domain/PayStationImpl.java
+++ src/paystation/domain/PayStationImpl.java
@@ -7,11 +7,9 @@ import java.util.HashMap;
  *
  * Responsibilities:
  *
- * 1) Accept payment; 
- * 2) Calculate parking time based on payment; 
- * 3) Know earning, parking time bought; 
- * 4) Issue receipts; 
- * 5) Handle buy and cancel events.
+ * 1) Accept payment; 2) Calculate parking time based on payment; 3) Know
+ * earning, parking time bought; 4) Issue receipts; 5) Handle buy and cancel
+ * events.
  *
  * This source code is from the book "Flexible, Reliable Software: Using
  * Patterns and Agile Development" published 2010 by CRC Press. Author: Henrik B
@@ -22,18 +20,26 @@ import java.util.HashMap;
  * purposes. For any commercial use, see http://www.baerbak.com/
  */
 public class PayStationImpl implements PayStation {
-    
+
     private int insertedSoFar;
     private int timeBought;
-    
+    private int nNickels = 0;
+    private int nDimes = 0;
+    private int nQuarters = 0;
 
     @Override
     public void addPayment(int coinValue)
             throws IllegalCoinException {
         switch (coinValue) {
-            case 5: break;
-            case 10: break;
-            case 25: break;
+            case 5:
+                nNickels++;
+                break;
+            case 10:
+                nDimes++;
+                break;
+            case 25:
+                nQuarters++;
+                break;
             default:
                 throw new IllegalCoinException("Invalid coin: " + coinValue);
         }
@@ -53,39 +59,48 @@ public class PayStationImpl implements PayStation {
         return r;
     }
 
+    /**
+     * Cancel the present transaction. Resets the paystation for a new
+     * transaction.
+     *
+     * @return A Map defining the coins returned to the user. 
+     * The key is the coin type and the associated value is the number of these 
+     * coins that are returned. 
+     * The Map object is never null even if no coins are returned. 
+     * The Map will only contain only keys for coins to be returned.
+     * The Map will be cleared after a cancel or buy.
+     */
+    
     @Override
     public HashMap<Integer, Integer> cancel() {
         Integer Zero = new Integer(0);
         Integer Five = new Integer(5);
         Integer Ten = new Integer(10);
         Integer Twentyfive = new Integer(25);
-        
+
         HashMap<Integer, Integer> coinsInChange;
         coinsInChange = new HashMap<Integer, Integer>();
-        
+
         // Calculate number of quarters returned
-        Integer nQuarters = new Integer(insertedSoFar/25);
-        insertedSoFar = insertedSoFar % 25;
-        
+        //Integer nQuarters = new Integer(insertedSoFar/25);
+        //insertedSoFar = insertedSoFar % 25;
         // Calculate number of dimes returned
-        Integer nDimes = new Integer(insertedSoFar/10);
-        insertedSoFar = insertedSoFar % 10;
-        
+        //Integer nDimes = new Integer(insertedSoFar/10);
+        //insertedSoFar = insertedSoFar % 10;
         // Calculate number of nickels returned
-        Integer nNickels = new Integer(insertedSoFar/5);// Assume DNE 0 < insertedSoFar < 5
-        
+        //Integer nNickels = new Integer(insertedSoFar/5);// Assume DNE 0 < insertedSoFar < 5
         // Return correct number of each coin in Map
         coinsInChange.put(Five, nNickels);
         coinsInChange.put(Ten, nDimes);
         coinsInChange.put(Twentyfive, nQuarters);
-        
+
         reset();
-        
+
         return coinsInChange;
     }
-    
+
     private void reset() {
-        timeBought = insertedSoFar = 0;
+        timeBought = insertedSoFar = nNickels = nDimes = nQuarters = 0;
     }
 
     @Override
